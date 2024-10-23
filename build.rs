79b33e0cccfe5c7335af91a90d4cc8e2a1a50bb1
Joshua Willis <joshua.willis@intellitect.com>
2024-10-22 23:44:29 -0700

    Clean up build script

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 use copy_to_output::copy_to_output;
 
 fn main() {
-    copy_to_output("src/static", &std::env::var("PROFILE").unwrap()).unwrap();
+    let env_profile = &std::env::var("PROFILE")
+        .expect("Unable to access PROFILE environment variable");
+    copy_to_output("src/static", env_profile).unwrap();
 }
